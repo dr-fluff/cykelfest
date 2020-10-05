54f1540133705552adb3e5677f19f4088163eca2
Oliver Gustavsson <oliver.gustavssons@gmail.com>
2020-10-05 18:43:26 +0200

    deside route almost don

--- main.py
+++ main.py
@@ -18,11 +18,17 @@ PLACE1 = 12
 PLACE2 = 13
 PLACE3 = 14
 
+# Meal constants
+STARTER = 1
+MAIN_COURSE = 2
+DESERT = 3
 
-#Defines a class "pair", contact info and preferences
+
+# Defines a class "pair", contact info and preferences
 class Pair:
 
-    def __init__(self, pairnr, name, email, phonenr, adress, alergy, teammember,alchol, tocook, place1, place2, place3):
+    def __init__(self, pairnr, name, email, phonenr, adress, alergy, teammember, alchol, tocook, place1, place2,
+                 place3):
         self.pairnr = pairnr
         self.name = name
         self.email = email
@@ -36,6 +42,7 @@ class Pair:
         self.place2 = place2
         self.place3 = place3
 
+
 # Read csv file and return a 2d array width person
 def Read_file(File_path):
     with open(File_path, 'r') as csvfile:
@@ -43,24 +50,22 @@ def Read_file(File_path):
         pairs = []
 
         for row in spamreader:
-            pair = Pair(row[PAIRNR],row[NAME],row[EMAIL],row[PHONENR],row[ADRESS],row[ALERGY],row[TEAMMEMBER],row[ALCHOL], row[TOCOOK], -1,-1,-1)
+            pair = Pair(row[PAIRNR], row[NAME], row[EMAIL], row[PHONENR], row[ADRESS], row[ALERGY], row[TEAMMEMBER],
+                        row[ALCHOL], row[TOCOOK], -1, -1, -1)
             pairs.append(pair)
 
         del pairs[0]
 
         for p in pairs:
-            #abc = "-1"
-            print(p.tocook, type(p.tocook))
-            print(int(p.tocook), type(p.tocook))
-
-            #p.pairnr = int(p.pairnr,base=10)
+            p.tocook = int(p.tocook)
 
     return pairs
 
-# Send an email to recipent with message
+
+# Send an email to recipient with message
 def send_email(recipient, message):
     sender = "max@idermark.com"
-    port = 465 #for ssl
+    port = 465  # for ssl
     password = "asd"
 
     # Create a secure SSL context
@@ -73,15 +78,13 @@ def send_email(recipient, message):
 
 # Decide all
 def decide_meal(pairs):
-
     unasigned_tocook = []
     meal_1 = []
     meal_2 = []
     meal_3 = []
 
     for p in pairs:
-
-        if p.tocook == -1:
+        if p.tocook == -1 or p.tocook == 0:
             unasigned_tocook.append(p)
         elif p.tocook == 1:
             meal_1.append(p)
@@ -97,7 +100,6 @@ def decide_meal(pairs):
         if amount_of_people_per_meal > len(meal_1):
             p.tocook = 1
             meal_1.append(p)
-            print(p.name)
         elif amount_of_people_per_meal > len(meal_2):
             p.tocook = 2
             meal_2.append(p)
@@ -113,7 +115,7 @@ def decide_meal(pairs):
     for m in meal_3:
         pairs.append(m)
 
-    strike_pair = adress_check(pairs)
+    strike_pair = address_check(pairs)
     if len(strike_pair) > 0:
         i = 0
         for s in strike_pair:
@@ -139,22 +141,22 @@ def decide_meal(pairs):
                             pairs = swap_meal(pairs, s, p)
                             break
 
-            i +=1
+            i += 1
 
     return pairs
 
-def adress_check(pairs):
 
+def address_check(pairs):
     pairs_meal_1 = []
     pairs_meal_2 = []
     pairs_meal_3 = []
 
     for pair in pairs:
-        if pair.tocook == 1:
+        if pair.tocook == STARTER:
             pairs_meal_1.append(pair)
-        elif pair.tocook == 2:
+        elif pair.tocook == MAIN_COURSE:
             pairs_meal_2.append(pair)
-        elif pair.tocook == 3:
+        elif pair.tocook == DESERT:
             pairs_meal_3.append(pair)
 
     strike_pairs = []
@@ -174,23 +176,22 @@ def adress_check(pairs):
             if (a.adress.lower() == b.adress.lower()) and (a.pairnr != b.pairnr):
                 strike_pairs.append(b)
 
-    for s in strike_pairs:
-        print("Strike pairs = ", s.name)
-
-    print("Strike pair lengh: ", len(strike_pairs))
+    if len(strike_pairs) > 0:
+        print("multiple pairs using same address")
+        for s in strike_pairs:
+            print(s.pairnr, "Name: ", s.name, "Address: ", s.adress)
 
     return strike_pairs
 
+
+# Help function for swapping meals
 def swap_meal(pairs, pair1, pair2):
-
-    print(pair1.name, " har nr ",pair1.tocook, pair2.name, "har nr " ,pair2.tocook)
-
     i = 0
     for p in pairs:
         if p.pairnr == pair1.pairnr:
-                del pairs[i]
+            del pairs[i]
         if p.pairnr == pair2.pairnr:
-                del pairs[i]
+            del pairs[i]
         i += 1
 
     meal_for_pair1 = pair1.tocook
@@ -199,30 +200,84 @@ def swap_meal(pairs, pair1, pair2):
     pair1.tocook = meal_for_pair2
     pair2.tocook = meal_for_pair1
 
-    print(meal_for_pair1,meal_for_pair2)
-
-    print(pair1.name, " har nr ",pair1.tocook, pair2.name, "har nr " ,pair2.tocook)
-
     pairs.append(pair1)
     pairs.append(pair2)
 
-    print("Swapped ", pair1.name , " with ", pair2.name , ".")
+    print("Swapped ", pair1.name, " with ", pair2.name, ".")
 
     return pairs
 
-# Decide everyone's route
-def decide_route(p):
-    list.append(p,2)
 
-    return p
+# Decide everyone's route
+def decide_route(pairs):
+    s = []
+    m = []
+    d = []
+
+    for p in pairs:
+        if p.tocook == STARTER:
+            s.append(p)
+        elif p.tocook == MAIN_COURSE:
+            m.append(p)
+        elif p.tocook == DESERT:
+            d.append(p)
+
+    gg = [[[]]]
+    for i in range(3):
+        groups_placement = group_placement(s, m, d, i + 1)
+        gg.append(groups_placement)
+
+    return gg
+
+
+# Help function for deciding routs
+def group_placement(s, m, d, what_list):
+    list1 = []
+    list2 = []
+    list3 = []
+
+    if what_list == STARTER:
+        list1 = s
+        list2 = m
+        list3 = d
+    elif what_list == MAIN_COURSE:
+        list1 = m
+        list2 = d
+        list3 = s
+    elif what_list == DESERT:
+        list1 = d
+        list2 = s
+        list3 = m
+
+    group_group = [[]]
+    j = 0
+    for i in list1:
+        g = []
+        g.append(i)
+
+        if j + 1 > len(list2) - 1:
+            g.append(list2[len(list2) - 1 - j])
+        else:
+            g.append(list2[j + 1])
+
+        if j + 2 > len(list3) - 1:
+            g.append(list3[len(list3) - 2 - j])
+        else:
+            g.append(list3[j + 2])
+
+        group_group.append(g)
+
+        j += 1
+
+    return group_group
 
 
 def main():
-    pairs = Read_file("Test.csv")
+    pairs = Read_file("../Test.csv")
 
     pairs = decide_meal(pairs)
 
-    for p in pairs:
-        print(p.pairnr, " Namn: ", p.name, "Adress: ", p.adress, "Måltid: ", p.tocook)
+    meal_groups = decide_route(pairs)
+
 
 main()
